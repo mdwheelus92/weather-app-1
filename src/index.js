0af218326ed21678691dcca2d2cb0a98d3dda04b
mdwheelus92 <michellewheelus@gmail.com>
2022-10-28 14:39:49 -0700

    Added JS for current day/time

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) hours = `0${hours}`;
+  let minutes = date.getMinutes();
+  if (minutes < 10) minutes = `0${minutes}`;
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day}, ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
@@ -5,13 +24,18 @@ function displayTemperature(response) {
   let windElement = document.querySelector("#wind");
   let humidityElement = document.querySelector("#humidity");
   let descriptionElement = document.querySelector("#description");
+  let dateElement = document.querySelector("#day");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   humidityElement.innerHTML = response.data.main.humidity;
   descriptionElement.innerHTML = response.data.weather[0].description;
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 let apiKey = "291775067f631eae270654eeeac354a1";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Mammoth Lakes&appid=${apiKey}&units=imperial`;
 
 axios.get(apiUrl).then(displayTemperature);
+
+//let dateElement = document.querySelector("#day");
+//dateElement.innerHTML = 1000 * response.data.dt;
