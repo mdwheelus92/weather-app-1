756b22057bce6590cdc3b8d50a71ff594e5a7244
mdwheelus92 <michellewheelus@gmail.com>
2022-10-29 16:58:29 -0700

    Added JS for current weather icon

--- src/index.js
+++ src/index.js
@@ -25,17 +25,24 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let descriptionElement = document.querySelector("#description");
   let dateElement = document.querySelector("#day");
+  let iconElement = document.querySelector("#icon");
+  let currentConditions = response.data.weather[0].icon;
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   humidityElement.innerHTML = response.data.main.humidity;
   descriptionElement.innerHTML = response.data.weather[0].description;
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${currentConditions}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
+
+let city = "Mammoth Lakes";
 let apiKey = "291775067f631eae270654eeeac354a1";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Mammoth Lakes&appid=${apiKey}&units=imperial`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
 
 axios.get(apiUrl).then(displayTemperature);
-
-//let dateElement = document.querySelector("#day");
-//dateElement.innerHTML = 1000 * response.data.dt;
